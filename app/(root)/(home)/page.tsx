1faec1d77e66aa41653346a139256d2ea183bd1d
Timothy Roch <timothyroch@gmail.com>
2024-08-11 14:13:13 -0400

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -8,7 +8,7 @@ const now = new Date();
   const date = formatter.format(new Date());  return (
     <section className="flex size-full flex-col gap-10 text-white">
 <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
-<div className="flex h-full flex-col items-center max-md:px-5 max-md:py-8 lg:p-11">
+<div className="flex h-full flex-col justify-center max-md:px-5 max-md:py-8 lg:p-11">
 
 <div className="flex flex-col gap-2">
   <h1 className="text-4xl font-extrabold lg:text-7xl">
